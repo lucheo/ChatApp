3a7de58ca5a1dd7387755fe30978167e4fb169f4
Lucheo Tombini Filho <lucheo@Lucheos-MacBook-Pro.local>
2018-01-26 18:37:51 -0200

    Login UI completed

---
+++ ChatApp/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  ChatApp
+//
+//  Created by Lucheo Tombini Filho on 26/01/18.
+//  Copyright © 2018 Lucheo Tombini Filho. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ ChatApp/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  ChatApp
+//
+//  Created by Lucheo Tombini Filho on 26/01/18.
+//  Copyright © 2018 Lucheo Tombini Filho. All rights reserved.
+//
+
+import Foundation
+// Segues
+
+let TO_LOGIN = "toLogin"
+
