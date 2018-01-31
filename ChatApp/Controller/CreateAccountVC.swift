c1adf1e01800c6aa5f4c26a01c97da00dbb02211
Lucheo Tombini Filho <lucheo@Lucheos-MacBook-Pro.local>
2018-01-31 16:34:36 -0200

    Register User done

--- ChatApp/Controller/CreateAccountVC.swift
+++ ChatApp/Controller/CreateAccountVC.swift
@@ -9,16 +9,37 @@
 import UIKit
 
 class CreateAccountVC: UIViewController {
-
+    
+    
+    @IBOutlet weak var userImg: UIImageView!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func pickBgColorPressed(_ sender: Any) {
+    }
+    
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
     @IBAction func closePressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
     
+    @IBAction func createAccountPressed(_ sender: Any) {
+        guard let email = emailField.text , emailField.text != "" else { return }
+        guard let pass = passwordField.text , passwordField.text != "" else { return }
+        
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            if success {
+                print("registered user!")
+            }
+        }
+        
+    }
 }
---
+++ ChatApp/View/RoundedButton.swift
@@ -0,0 +1,33 @@
+//
+//  RoundedButton.swift
+//  ChatApp
+//
+//  Created by Lucheo Tombini Filho on 31/01/18.
+//  Copyright © 2018 Lucheo Tombini Filho. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    
+
+}
