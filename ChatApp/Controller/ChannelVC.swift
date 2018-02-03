e0778e979a4f5cb1054cb5186332bc47fd2e1a6f
Lucheo Tombini Filho <lucheo@Lucheos-MacBook-Pro.local>
2018-02-03 17:54:10 -0200

    Channel Cell done

--- ChatApp/Controller/ChannelVC.swift
+++ ChatApp/Controller/ChannelVC.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class ChannelVC: UIViewController {
+class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var userImg: CircleImage!
     @IBOutlet weak var loginBtn: UIButton!
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue) {}
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
 
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
         NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(notif:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
@@ -52,6 +55,26 @@ class ChannelVC: UIViewController {
             userImg.backgroundColor = UIColor.clear
         }
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "ChannelCell", for: indexPath) as? ChannelCell {
+            let channel = MessageService.instance.channels[indexPath.row]
+            cell.configureCell(channel: channel)
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MessageService.instance.channels.count
+    }
+    
+    
     
 
 }
---
+++ ChatApp/Model/Channel.swift
@@ -0,0 +1,17 @@
+//
+//  Channel.swift
+//  ChatApp
+//
+//  Created by Lucheo Tombini Filho on 03/02/18.
+//  Copyright © 2018 Lucheo Tombini Filho. All rights reserved.
+//
+
+import Foundation
+
+
+struct Channel {
+    
+    public private(set) var channelTitle: String
+    public private(set) var channelDescription: String
+    public private(set) var id: String
+}
